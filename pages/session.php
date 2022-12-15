32fe6408ab01841ac5320952343d228a504e7665
Prince Belaro <118361996+reina225@users.noreply.github.com>
2022-12-15 09:31:30 +0800

    changing session

--- pages/session.php
+++ pages/session.php
@@ -6,6 +6,7 @@
 	//create a new function to check if the session variable member_id is on set
 	function logged_in() {
 		return isset($_SESSION['MEMBER_ID']);
+		return isset($_SESSION['CUST_ID']);
         
 	}
 	//this function if session member is not set then it will be redirected to index.php
